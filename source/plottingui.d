45d3621d1140d3a89be4c4def0e70c7690384b06
Isaac Robinson <awesomeisaac2000@gmail.com>
2021-09-25 11:23:51 -0600

    Fix stuff

--- source/plottingui.d
+++ source/plottingui.d
@@ -1,40 +1,22 @@
 module plottingui;
 
-import gio.Application : GioApplication = Application;
-import gtk.Application;
-import gtk.ApplicationWindow;
-import gtk.Button;
-import gtk.Widget;
-import gtk.DrawingArea;
-import cairo.Context;
-import cairo.Surface;
-import ggplotd.ggplotd;
-import ggplotd.bounds: Bounds, width, height;
-
-
-class TransformableScale {
-    double xCenterShift = 0;
-    double yCenterShift = 0;
-    double zoomFactor = 1;
-
-    Context scale(Context c, Bounds b, double pixelWidth, double pixelHeight) {
-        c.translate(0, pixelHeight);
-        c.scale(pixelWidth / b.width, -pixelHeight / b.height);
-        c.translate(-b.min_x, -b.min_y);
-        //c.translate(-b.width / 2, -b.height / 2);
-        //c.scale(zoomFactor, zoomFactor);
-        //c.translate((b.width / 2) * zoomFactor, (b.height / 2) * zoomFactor);
-        //c.translate(-xCenterShift, -yCenterShift);
-
-        return c;
-    }
+private {
+    import gio.Application : GioApplication = Application;
+    import gtk.Application;
+    import gtk.ApplicationWindow;
+    import gtk.Button;
+    import gtk.Widget;
+    import gtk.DrawingArea;
+    import cairo.Context;
+    import cairo.Surface;
+    import ggplotd.ggplotd;
+    import ggplotd.axes: XAxis, YAxis;
 }
 
 class PlotWidget: DrawingArea {
     private:
 
     GGPlotD _plot;
-    TransformableScale scale;
 
     protected:
 
@@ -54,7 +36,6 @@ class PlotWidget: DrawingArea {
 
     this(GGPlotD plot) {
         this._plot = plot;
-        //plot.put(&(scale.scale));
         addOnDraw(&drawHandler);
     }
 
